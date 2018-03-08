3126987b52ff655bdfa70a11bc33bcc41d4af082
Eric Grynberg <eric.grynberg@gmail.com>
2018-03-07 23:19:47 -0300

    Commit inicial

---
+++ inspectorx2/server/config.js
@@ -0,0 +1,6 @@
+// server/config.js
+module.exports = {
+  AUTH0_DOMAIN: 'inspx2.auth0.com', // e.g., kmaida.auth0.com
+  AUTH0_API_AUDIENCE: 'http://localhost:8083/api/', // e.g., 'http://localhost:8083/api/'
+  MONGO_URI: process.env.MONGO_URI || 'mongodb://inspx2:159358@ds257808.mlab.com:57808/db_inspx'
+};
---
+++ inspectorx2/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule, Title } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+
+import { AppComponent } from './app.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [
+  	Title
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
