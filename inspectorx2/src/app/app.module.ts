1de6aae5468f09c583338e761c5115a374976ec9
Eric Grynberg <eric.grynberg@gmail.com>
2018-03-08 00:58:19 -0300

    Header e Footer preparados

--- inspectorx2/src/app/app.module.ts
+++ inspectorx2/src/app/app.module.ts
@@ -4,11 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
